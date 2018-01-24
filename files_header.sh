3728adad1d4fb91e402c545312183db44f23b396
Carlos Cabanero <cc@blink.sh>
2018-01-23 20:53:27 -0500

    Header checker script

---
+++ files_header.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+echo "Checking all files for the header:"
+cat header
+echo "Please wait..."
+
+errors=false
+for i in $(find . -path "./Frameworks/*" -prune -name "*.h" -o -name "*.m"); do
+    DIFF=$(diff <(head -n 30 header) <(head -n 30 $i))
+    if [ "$DIFF" != "" ]; then { echo -e "\033[0;31mERROR - \033[0m$i"; errors=true; } fi
+done
+
+if [ $errors = true ]; then
+    echo "Errors processing input"
+    exit 1
+fi
+
+echo "Success!"
