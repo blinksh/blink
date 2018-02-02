e835d569cfb84fa8c7af39ef4f61864a029f6457
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2018-02-02 13:09:24 +0100

    Bug fix in get-frameworks.sh

--- get_frameworks.sh
+++ get_frameworks.sh
@@ -28,8 +28,8 @@ curl -OL $GHROOT/build-protobuf/releases/download/$PROTOBF_VER/protobuf-$PROTOBF
 ( tar -zxf protobuf-*.tar.gz && cp protobuf-*/lib/libprotobuf.a ./lib/ && rm -rf protobuf-* ) || { echo "Protobuf framework failed to download"; exit 1; }
 # ios_system
 echo "Downloading ios_system.framework.zip"
-curl -OL $HHROOT/ios_system/releases/download/v$IOS_SYSTEM_VER/ios_system.framework.tar.gz
-( tar -xzf ios_system.framework.tar.gz && rm ios_system.framework.tar.gz && mv release/* . ) || { echo "ios_system failed to download"; exit 1; }
+curl -OL $HHROOT/ios_system/releases/download/v$IOS_SYSTEM_VER/release.tar.gz
+( tar -xzf release.tar.gz && rm release.tar.gz && mv release/* . ) || { echo "ios_system failed to download"; exit 1; }
 )
 
 # We need ios_system for the sources of curl_static too:
