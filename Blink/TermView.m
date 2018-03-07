218f2e06ef98f02a9076cb070e76aefde838676c
Yury Korolev <yury.korolev@gmail.com>
2018-03-07 16:57:53 +0300

    Fix for barrier

--- Blink/TermView.m
+++ Blink/TermView.m
@@ -268,9 +268,9 @@
 
 - (void)writeB64:(NSData *)data
 {
-  dispatch_async(_jsQueue, ^{
+  dispatch_barrier_async(_jsQueue, ^{
     NSString *jsScript = term_writeB64(data);
-    dispatch_barrier_async(dispatch_get_main_queue(), ^{
+    dispatch_async(dispatch_get_main_queue(), ^{
       [_webView evaluateJavaScript: jsScript completionHandler:nil];
     });
   });
