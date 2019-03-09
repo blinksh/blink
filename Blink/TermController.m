aff79894851f73157365a3b23f4daf3ac2d32587
Yury Korolev <yury.korolev@gmail.com>
2019-03-09 12:59:14 +0300

    Fix missing mosh host in siri shortcuts. #592

--- Blink/TermController.m
+++ Blink/TermController.m
@@ -129,15 +129,12 @@ NSString * const BKUserActivityCommandLineKey = @"com.blink.cmdline.key";
   if ([_session isRunningCmd] || !cmdLine) {
     return;
   }
+  
   char ctrlA = 'a' - 'a' + 1;
   char ctrlK = 'k' - 'a' + 1;
   // delete all input on current line - ctrl+a ctrl+k
   // run command
-  if (self.userActivity) {
-    [_termDevice write:[NSString stringWithFormat:@"%c%c%@\n", ctrlA, ctrlK, cmdLine]];
-  } else {
-    self.userActivity = activity;
-  }
+  [_termDevice write:[NSString stringWithFormat:@"%c%c%@\n", ctrlA, ctrlK, cmdLine]];
 }
 
 - (void)viewDidLoad
