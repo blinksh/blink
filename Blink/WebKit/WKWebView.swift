34a30176273f9138388e02679ea809be84426107
Yury Korolev <yury.korolev@gmail.com>
2019-09-21 14:02:15 +0300

    Report wheel

--- Blink/WebKit/WKWebView.swift
+++ Blink/WebKit/WKWebView.swift
@@ -48,6 +48,8 @@ class UIScrollViewWithoutHitTest: UIScrollView {
   private var _wkWebView: WKWebView? = nil
   private var _scrollView = UIScrollViewWithoutHitTest()
   private var _jsScrollerPath: String
+  private var _panGestureRecognizer = UIPanGestureRecognizer()
+  private var _tapGestureRecognizer = UITapGestureRecognizer()
   
   @objc init(jsScrollerPath: String) {
     _jsScrollerPath = jsScrollerPath
@@ -59,6 +61,16 @@ class UIScrollViewWithoutHitTest: UIScrollView {
     _scrollView.keyboardDismissMode = .interactive
     _scrollView.delaysContentTouches = false
     _scrollView.delegate = self
+    
+    _panGestureRecognizer.minimumNumberOfTouches = 2
+    _panGestureRecognizer.maximumNumberOfTouches = 2
+    _panGestureRecognizer.addTarget(self, action: #selector(_onPan(_:)))
+    _panGestureRecognizer.delegate = self
+    
+    _tapGestureRecognizer.numberOfTapsRequired = 1
+    _tapGestureRecognizer.numberOfTouchesRequired = 1
+    _tapGestureRecognizer.addTarget(self, action: #selector(_onTap(_:)))
+    _tapGestureRecognizer.delegate = self
   }
   
   func willMove(to view: UIView?) {
@@ -71,19 +83,68 @@ class UIScrollViewWithoutHitTest: UIScrollView {
   
   func didMove(to view: UIView?) {
     if let webView = view as? WKWebView {
-      webView.addSubview(_scrollView)
-      webView.addGestureRecognizer(_scrollView.panGestureRecognizer)
-      webView.configuration.userContentController.add(self, name: "wkScroller")
-      
       webView.scrollView.delaysContentTouches = false;
       webView.scrollView.canCancelContentTouches = false;
       webView.scrollView.isScrollEnabled = false;
       webView.scrollView.panGestureRecognizer.isEnabled = false;
       
+      
+      webView.addSubview(_scrollView)
+      webView.addGestureRecognizer(_scrollView.panGestureRecognizer)
+      webView.configuration.userContentController.add(self, name: "wkScroller")
+      webView.addGestureRecognizer(_panGestureRecognizer)
+      webView.addGestureRecognizer(_tapGestureRecognizer)
+
+      
       _wkWebView = webView
     } else {
       _scrollView.removeFromSuperview()
     }
+  }
+  
+  private var _reportedY:CGFloat = 0
+  
+  @objc func _onPan(_ recognizer: UIPanGestureRecognizer) {
+    let point = recognizer.location(in: recognizer.view)
+    
+    switch recognizer.state {
+    case .began:
+      _scrollView.panGestureRecognizer.dropTouches()
+      _scrollView.isScrollEnabled = false
+      _scrollView.showsVerticalScrollIndicator = false
+      _reportedY = point.y
+    case .changed:
+      let dY = point.y - _reportedY;
+      if abs(dY) < 5 {
+        return
+      }
+      _reportedY = point.y
+      let deltaY = dY > 0 ? -1 : 1
+      let deltaX = 0 // hterm supports only deltaY for now
+      _wkWebView?.evaluateJavaScript("term_reportWheelEvent(\"wheel\", \(point.x), \(point.y), \(deltaX), \(deltaY));", completionHandler: nil)
+    case .ended: fallthrough
+    case .cancelled:
+      _scrollView.isScrollEnabled = true
+      _scrollView.showsVerticalScrollIndicator = true
+    default: break
+    }
+  }
+  
+  @objc func _onTap(_ recognizer: UITapGestureRecognizer) {
+    let point = recognizer.location(in: recognizer.view)
+    switch recognizer.state {
+    case .recognized:
+      _wkWebView?.evaluateJavaScript("term_reportMouseEvent(\"mousedown\", \(point.x), \(point.y));", completionHandler: nil)
+      _wkWebView?.evaluateJavaScript("term_reportMouseEvent(\"mouseup\", \(point.x), \(point.y));", completionHandler: nil)
+    default: break
+    }
+  }
+  
+}
+
+extension WKWebViewGesturesInteraction: UIGestureRecognizerDelegate {
+  func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+    return true
   }
 }
 
--- Resources/term.js
+++ Resources/term.js
@@ -5,7 +5,7 @@ hterm.defaultStorage = new lib.Storage.Memory();
 window.fontSizeDetectionMethod = 'canvas';
 
 function _postMessage(op, data) {
-  window.webkit.messageHandlers.interOp.postMessage({ op, data });
+  window.webkit.messageHandlers.interOp.postMessage({op, data});
 }
 
 hterm.Terminal.prototype.copyStringToClipboard = function(content) {
@@ -14,7 +14,7 @@ hterm.Terminal.prototype.copyStringToClipboard = function(content) {
   }
 
   document.getSelection().removeAllRanges();
-  _postMessage('copy', { content });
+  _postMessage('copy', {content});
 };
 
 // Speedup a little bit.
@@ -24,9 +24,8 @@ document.addEventListener('selectionchange', function() {
   _postMessage('selectionchange', term_getCurrentSelection());
 });
 
-
 hterm.Terminal.IO.prototype.sendString = function(string) {
-  _postMessage('sendString', { string });
+  _postMessage('sendString', {string});
 };
 
 hterm.msg = function() {}; // TODO: show messages
@@ -35,13 +34,16 @@ function _colorComponents(colorStr) {
   if (!colorStr) {
     return [0, 0, 0]; // Default is black
   }
-  
-  return colorStr.replace(/[^0-9,]/g, '').split(',').map(s => parseInt(s));
+
+  return colorStr
+    .replace(/[^0-9,]/g, '')
+    .split(',')
+    .map(s => parseInt(s));
 }
 
 // Before we fully load hterm. We set options here.
 var _prefs = new hterm.PreferenceManager('blink');
-var t = { prefs_: _prefs }; // <- `t` will become actual hterm instance after decorate.
+var t = {prefs_: _prefs}; // <- `t` will become actual hterm instance after decorate.
 
 function term_set(key, value) {
   _prefs.set(key, value);
@@ -65,16 +67,17 @@ function term_setup() {
     t.setCursorVisible(true);
 
     t.io.onTerminalResize = function(cols, rows) {
-      _postMessage('sigwinch', { cols, rows });
+      _postMessage('sigwinch', {cols, rows});
     };
 
     var size = {
       cols: t.screenSize.width,
       rows: t.screenSize.height,
     };
-    document.body.style.backgroundColor = t.scrollPort_.screen_.style.backgroundColor;
+    document.body.style.backgroundColor =
+      t.scrollPort_.screen_.style.backgroundColor;
     var bgColor = _colorComponents(t.scrollPort_.screen_.style.backgroundColor);
-    _postMessage('terminalReady', { size, bgColor });
+    _postMessage('terminalReady', {size, bgColor});
 
     t.keyboard.characterEncoding = 'raw'; // we are UTF8. Fix for #507
     t.uninstallKeyboard();
@@ -87,12 +90,17 @@ function term_init() {
   term_setupDefaults();
   try {
     applyUserSettings();
-//    var bgColor = term_get('background-color');
-//    document.body.style.backgroundColor = bgColor;
-//    document.body.parentNode.style.backgroundColor = bgColor;
+    //    var bgColor = term_get('background-color');
+    //    document.body.style.backgroundColor = bgColor;
+    //    document.body.parentNode.style.backgroundColor = bgColor;
     waitForFontFamily(term_setup);
   } catch (e) {
-    _postMessage('alert', {title: 'Error', message: "Failed to setup theme. Please check syntax of your theme.\n" + e.toString()})
+    _postMessage('alert', {
+      title: 'Error',
+      message:
+        'Failed to setup theme. Please check syntax of your theme.\n' +
+        e.toString(),
+    });
     term_setup();
   }
 }
@@ -102,7 +110,7 @@ function term_write(data) {
 }
 
 function term_paste(str) {
-  t.onPaste_({text: str || ""});
+  t.onPaste_({text: str || ''});
 }
 
 var term_write_b64 = null;
@@ -113,20 +121,20 @@ if (typeof TextDecoder !== 'undefined') {
     var bytes = base64js.toByteArray(b64str); // b64_to_uint8_array(b64str);
     var data = _utf8TextDecoder.decode(bytes);
     t.interpret(data);
-  }
+  };
 } else {
   // ios 10 support
   var _fileReader = new FileReader();
   var _blobOptions = {type: 'text/plain; charset=utf-8'};
-  
-  _fileReader.onload = function () {
+
+  _fileReader.onload = function() {
     t.interpret(_fileReader.result);
   };
-  
+
   term_write_b64 = function term_write_b64_blob(b64str) {
     var bytes = base64js.toByteArray(b64str); // b64_to_uint8_array(b64str);
     _fileReader.readAsText(new Blob([bytes], _blobOptions));
-  }
+  };
 }
 
 function b64_to_uint8_array(b64Str) {
@@ -139,60 +147,67 @@ function b64_to_uint8_array(b64Str) {
   return res;
 }
 
-
 function term_clear() {
   t.clear();
 }
 
 function term_setIme(str) {
   var length = lib.wc.strWidth(str);
-  
+
   var scrollPort = t.scrollPort_;
   var ime = t.ime_;
   ime.textContent = str;
-  
+
   if (length === 0) {
     return;
   }
 
   ime.style.backgroundColor = lib.colors.setAlpha(t.getCursorColor(), 1);
-  ime.style.color = scrollPort.getBackgroundColor()
-  
+  ime.style.color = scrollPort.getBackgroundColor();
+
   var screenCols = t.screenSize.width;
   var cursorCol = t.screen_.cursorPosition.column;
-  
+
   ime.style.bottom = 'auto';
   ime.style.top = 'auto';
-  
+
   if (length >= screenCols) {
     // We are wider than the screen
     ime.style.left = '0px';
     ime.style.right = '0px';
     if (t.screen_.cursorPosition.row < t.screenSize.height * 0.8) {
-      ime.style.top = 'calc(var(--hterm-charsize-height) * (var(--hterm-cursor-offset-row) + 1))'
+      ime.style.top =
+        'calc(var(--hterm-charsize-height) * (var(--hterm-cursor-offset-row) + 1))';
     } else {
-      ime.style.top = 'calc(var(--hterm-charsize-height) * (var(--hterm-cursor-offset-row) - ' + (Math.floor(length / (screenCols + 1))) + ' - 1))'
+      ime.style.top =
+        'calc(var(--hterm-charsize-height) * (var(--hterm-cursor-offset-row) - ' +
+        Math.floor(length / (screenCols + 1)) +
+        ' - 1))';
     }
-  } else if (cursorCol + length <= screenCols ) {
+  } else if (cursorCol + length <= screenCols) {
     // we are inlined
-    ime.style.left = 'calc(var(--hterm-charsize-width) * var(--hterm-cursor-offset-col))';
-    ime.style.top = 'calc(var(--hterm-charsize-height) * var(--hterm-cursor-offset-row))';
+    ime.style.left =
+      'calc(var(--hterm-charsize-width) * var(--hterm-cursor-offset-col))';
+    ime.style.top =
+      'calc(var(--hterm-charsize-height) * var(--hterm-cursor-offset-row))';
     ime.style.right = 'auto';
   } else if (t.screen_.cursorPosition.row == 0) {
     // we are at the end of line but need more space at the bottom
-    ime.style.top = 'calc(var(--hterm-charsize-height) * (var(--hterm-cursor-offset-row) + 1))';
+    ime.style.top =
+      'calc(var(--hterm-charsize-height) * (var(--hterm-cursor-offset-row) + 1))';
     ime.style.left = 'auto';
     ime.style.right = '0px';
   } else {
     // we are at the end of line but need more space at the top
-    ime.style.top = 'calc(var(--hterm-charsize-height) * (var(--hterm-cursor-offset-row) - 1))';
+    ime.style.top =
+      'calc(var(--hterm-charsize-height) * (var(--hterm-cursor-offset-row) - 1))';
     ime.style.left = 'auto';
     ime.style.right = '0px';
   }
-  var r = ime.getBoundingClientRect()
-  
-  const markedRect = ["{{", r.x, ",", r.y, "},{", r.width, ",", r.height, "}}"].join('');
-  
+  var r = ime.getBoundingClientRect();
+
+  const markedRect = `{{${r.x}, ${r.y}},{${r.width},${r.height}}}`;
+
   return {markedRect};
 }
 
@@ -208,18 +223,31 @@ function term_blur() {
   t.onFocusChange__(false);
 }
 
-function term_reportTouchInPoint(x, y) {
-  var mousedown = new MouseEvent("mousedown", {});
+function _setTermCoordinates(event, x, y) {
   // One based row/column stored on the mouse event.
-  mousedown.terminalRow = parseInt((y - t.scrollPort_.visibleRowTopMargin) /
-                           t.scrollPort_.characterSize.height) + 1;
-  mousedown.terminalColumn = parseInt(x /
-                              t.scrollPort_.characterSize.width) + 1;
-  t.onMouse(mousedown);
-  var mouseup = new MouseEvent("mouseup", {});
-  mouseup.terminalRow = mousedown.terminalRow;
-  mouseup.terminalColumn = mousedown.terminalColumn;
-  t.onMouse(mouseup);
+  event.terminalRow =
+    parseInt(
+      (y - t.scrollPort_.visibleRowTopMargin) /
+        t.scrollPort_.characterSize.height,
+    ) + 1;
+  event.terminalColumn = parseInt(x / t.scrollPort_.characterSize.width) + 1;
+}
+
+function term_reportTapInPoint(x, y) {
+  term_reportMouseEvent('mousedown', x, y);
+  term_reportMouseEvent('mouseup', x, y);
+}
+
+function term_reportMouseEvent(name, x, y) {
+  var event = new MouseEvent(name, {});
+  _setTermCoordinates(event, x, y);
+  t.onMouse(event);
+}
+
+function term_reportWheelEvent(name, x, y, deltaX, deltaY) {
+  var event = new WheelEvent(name, {deltaX, deltaY});
+  _setTermCoordinates(event, x, y);
+  t.onMouse(event);
 }
 
 function term_setWidth(cols) {
@@ -228,12 +256,12 @@ function term_setWidth(cols) {
 
 function term_increaseFontSize() {
   var size = t.getFontSize();
-  term_setFontSize((size + 1) + "px");
+  term_setFontSize(size + 1 + 'px');
 }
 
 function term_decreaseFontSize() {
   var size = t.getFontSize();
-  term_setFontSize((size - 1) + "px");
+  term_setFontSize(size - 1 + 'px');
 }
 
 function term_resetFontSize() {
@@ -254,7 +282,7 @@ function term_scale(scale) {
 
 function term_setFontSize(size) {
   term_set('font-size', size);
-  _postMessage('fontSizeChanged', { size: parseInt(size) });
+  _postMessage('fontSizeChanged', {size: parseInt(size)});
 }
 
 function term_setFontFamily(name, fontSizeDetectionMethod) {
@@ -287,18 +315,18 @@ function term_loadFontFromCss(url, name) {
 function term_getCurrentSelection() {
   const selection = document.getSelection();
   if (!selection || selection.rangeCount === 0) {
-    return { base: '', offset: 0, text: '' };
+    return {base: '', offset: 0, text: ''};
   }
-  
-  const r = selection.getRangeAt(0).getBoundingClientRect()
 
-  const rect = ["{{", r.x, ",", r.y, "},{", r.width, ",", r.height, "}}"].join('');
-  
+  const r = selection.getRangeAt(0).getBoundingClientRect();
+
+  const rect = `{{${r.x}, ${r.y}},{${r.width},${r.height}}}`;
+
   return {
     base: selection.baseNode.textContent,
     offset: selection.baseOffset,
     text: selection.toString(),
-    rect
+    rect,
   };
 }
 
@@ -308,48 +336,74 @@ function _modifySelectionByLine(direction) {
   var fOffset = selection.focusOffset;
   var aNode = selection.anchorNode;
   var aOffset = selection.anchorOffset;
-  
-  var dy = direction === 'left' ? -t.scrollPort_.characterSize.height : t.scrollPort_.characterSize.height;
+
+  var dy =
+    direction === 'left'
+      ? -t.scrollPort_.characterSize.height
+      : t.scrollPort_.characterSize.height;
   var dx = t.scrollPort_.characterSize.width;
   var range = selection.getRangeAt(0);
-  
+
   var topLeft = true;
   if (fNode === aNode) {
     topLeft = fOffset < aOffset;
   } else {
     topLeft = range.compareNode(selection.focusNode) !== Range.NODE_AFTER;
   }
-  
+
   if (topLeft) {
     // top left
     var rect = _filteredRects(range)[0];
-    var point = { x: rect.left, y: rect.top + Math.abs(dy) * 0.5 };
+    var point = {x: rect.left, y: rect.top + Math.abs(dy) * 0.5};
     var newRange = document.caretRangeFromPoint(point.x, point.y + dy);
     if (!newRange) {
-      selection.modify("extend", direction, 'line');
+      selection.modify('extend', direction, 'line');
     } else {
       if (newRange.startContainer.textContent.length <= newRange.startOffset) {
-        if (newRange.startContainer.nodeName === 'X-ROW' && newRange.startOffset === 0) {
-          selection.setBaseAndExtent(aNode, aOffset, newRange.startContainer, newRange.startOffset);
-          selection.modify("extend", 'left', 'character');
+        if (
+          newRange.startContainer.nodeName === 'X-ROW' &&
+          newRange.startOffset === 0
+        ) {
+          selection.setBaseAndExtent(
+            aNode,
+            aOffset,
+            newRange.startContainer,
+            newRange.startOffset,
+          );
+          selection.modify('extend', 'left', 'character');
         } else {
-          selection.setBaseAndExtent(aNode, aOffset, newRange.startContainer, Math.max(newRange.startOffset - 1, 0));
+          selection.setBaseAndExtent(
+            aNode,
+            aOffset,
+            newRange.startContainer,
+            Math.max(newRange.startOffset - 1, 0),
+          );
         }
       } else {
-        selection.setBaseAndExtent(aNode, aOffset, newRange.startContainer, newRange.startOffset);
+        selection.setBaseAndExtent(
+          aNode,
+          aOffset,
+          newRange.startContainer,
+          newRange.startOffset,
+        );
       }
     }
   } else {
     // bottom right
     var rects = _filteredRects(range);
     var rect = rects[rects.length - 1];
-    var point = { x: rect.right, y: rect.bottom - Math.abs(dy) * 0.5};
+    var point = {x: rect.right, y: rect.bottom - Math.abs(dy) * 0.5};
     var newRange = document.caretRangeFromPoint(point.x, point.y + dy);
     if (newRange == null) {
       point.x -= dx * 0.5;
     }
     newRange = document.caretRangeFromPoint(point.x, point.y + dy);
-    selection.setBaseAndExtent(aNode, aOffset, newRange.startContainer, newRange.startOffset);
+    selection.setBaseAndExtent(
+      aNode,
+      aOffset,
+      newRange.startContainer,
+      newRange.startOffset,
+    );
   }
 }
 
@@ -370,28 +424,28 @@ function term_modifySelection(direction, granularity) {
   if (!selection || selection.rangeCount === 0) {
     return;
   }
-  
+
   var fNode = selection.focusNode;
   var fOffset = selection.focusOffset;
   var aNode = selection.anchorNode;
   var aOffset = selection.anchorOffset;
-  
+
   if (granularity === 'line') {
     _modifySelectionByLine(direction);
     if (selection.isCollapsed) {
       selection.setBaseAndExtent(fNode, fOffset, aNode, aOffset);
       _modifySelectionByLine(direction);
     }
-    
+
     return;
   }
- 
-  selection.modify("extend", direction, granularity);
-  
+
+  selection.modify('extend', direction, granularity);
+
   // we collapse selection, so swap direction and rerun modification again
   if (selection.isCollapsed) {
     selection.setBaseAndExtent(fNode, fOffset, aNode, aOffset);
-    selection.modify("extend", direction, granularity);
+    selection.modify('extend', direction, granularity);
   }
 }
 
@@ -400,8 +454,13 @@ function term_modifySideSelection() {
   if (!selection || selection.rangeCount === 0) {
     return;
   }
-  
-  selection.setBaseAndExtent(selection.focusNode, selection.focusOffset, selection.anchorNode, selection.anchorOffset);
+
+  selection.setBaseAndExtent(
+    selection.focusNode,
+    selection.focusOffset,
+    selection.anchorNode,
+    selection.anchorOffset,
+  );
 }
 
 function term_cleanSelection() {
@@ -417,7 +476,7 @@ function waitForFontFamily(callback) {
   const families = fontFamily.split(/\s*,\s*/);
 
   WebFont.load({
-    custom: { families },
+    custom: {families},
     active: callback,
     inactive: callback,
   });
