a175c52fee0bf3261a95158b105d0f0b036622c0
Yury Korolev <yury.korolev@gmail.com>
2020-01-31 08:37:35 +0300

    Allow to map | key press. Refs #928

--- KB/Native/Model/KeyBindingAction.swift
+++ KB/Native/Model/KeyBindingAction.swift
@@ -151,6 +151,8 @@ enum KeyBindingAction: Codable, Identifiable {
       .hex("3C", comment: "Press <"),
       .hex("3E", comment: "Press >"),
       .hex("A7", comment: "Press §"),
+      .hex("7C", comment: "Press |"),
+      .hex("5C", comment: "Press \\"),
     ]
   }
   
