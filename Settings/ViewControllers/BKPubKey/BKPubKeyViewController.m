36076abeda1ca95315f15b392ea8eb7a5cf5ca26
Javier de Martín <javier@blink.sh>
2020-12-09 16:30:47 +0100

    Fixed unable to import private key from files #1106 (#1174)
    * Fixed unable to import private key from files #1106
    
    Missing NSURL.startAccessingSecurityScopedResource led to errors importing private keys from a file
    
    * Check if the request to access the file is valid
    
    * Fixed unable to import private key from files #1106

--- Settings/ViewControllers/BKPubKey/BKPubKeyViewController.m
+++ Settings/ViewControllers/BKPubKey/BKPubKeyViewController.m
@@ -363,10 +363,16 @@ enum SshKeyImportOrigin {
 - (void)documentPicker:(UIDocumentPickerViewController *)controller didPickDocumentsAtURLs:(NSArray<NSURL *> *)urls {
   
   for (NSURL *fileUrl in urls) {
+    // Make the resource we're trying to access accessible to the app.
+    if ([fileUrl startAccessingSecurityScopedResource] == NO) {
+      continue;
+    }
     
     NSString *keyString = [NSString stringWithContentsOfURL:fileUrl encoding:NSUTF8StringEncoding error:NULL];
-
+    [fileUrl stopAccessingSecurityScopedResource];
+    
     [self _importKeyFromString:keyString importOrigin:(FROM_FILE)];
+    
   }
 }
 
