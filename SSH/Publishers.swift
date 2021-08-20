7efbc231d18d90fff1c8bf8282acd0e25181bfa0
Carlos Cabanero <carlos@blink.sh>
2021-08-20 09:29:39 -0400

    Refactored Publisher lock
    The previous lock was failing in some specific cases as it was not able to
    reacquire and end up in a loop, or it would fail with an unfair_lock recursive
    abort.
    
    Two things need to happen for the cancel to work. We had to signal the flow not
    to continue, as the Cancels happen on different threads, and so when the Cancel
    moves up it will be scheduled but not executed on the other thread running the
    operation. The second is that when the Cancel is sent, we need to block it and
    ensure that it does not continue going up. Otherwise Combine may unlock while we
    are still awaiting for the RunLoop.
    
    To solve this, we are introduing a simpler Atomic value that controls the stop
    sequence, and a Sleep timer of roughly the same RunLoop wait time, to make sure
    that the flow does not continue.

--- SSH/Publishers.swift
+++ SSH/Publishers.swift
@@ -29,29 +29,32 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 
+import Atomics
 import Combine
 import Foundation
 import LibSSH
+
+
 
 // https://stackoverflow.com/questions/60624851/combine-framework-retry-after-delay
 // https://stackoverflow.com/questions/61557327/401-retry-mechanism-using-combine-publishers
 extension Publisher {
   func tryOperation<T, U>(_ operation: @escaping (U) throws -> T)
   -> AnyPublisher<T, Error> where Self == AnyPublisher<U, Error> {
-    let lock = UnfairLock()
-    var stop = false
+    let stop = ManagedAtomic<Bool>(false)
     
     func loop(_ session: U) -> AnyPublisher<T, Error> {
       return Just(session).tryMap { session in
-        lock.lock()
-        if stop {
-          throw SSHError(title: "Cancelled")
-        }
         let val = try operation(session)
-        lock.unlock()
+        if stop.load(ordering: .relaxed) {
+          throw SSHError(title: "Operation cancelled")
+        }
         return val
       }
       .tryCatch { error -> AnyPublisher<T, Error> in
+        if stop.load(ordering: .relaxed) {
+          throw SSHError(title: "Operation cancelled")
+        }
         switch error {
         case SSHError.again:
           Swift.print("tryOperation AGAIN")
@@ -60,7 +63,9 @@ extension Publisher {
           // This makes Combine process the "finished" messages before the current block is done,
           // slicing and stopping the flows.
           RunLoop.current.run(mode: .default, before: Date(timeIntervalSinceNow: 0.5))
-          lock.unlock()
+          if stop.load(ordering: .relaxed) {
+            throw SSHError(title: "Operation cancelled")
+          }
           return loop(session)
         default:
           throw error
@@ -70,53 +75,48 @@ extension Publisher {
     
     // Wrap in a loop so that only the specific operation is re-executed,
     // otherwise the whole flow will get up and cancelled.
-    return self.handleEvents(receiveCancel: {
-      lock.unlock()
-    }).flatMap { loop ($0) }
-    .handleEvents(receiveCancel: {
-      lock.spinLock()
-      stop = true
+    return self.flatMap { loop ($0) }.handleEvents(receiveCancel: {
+      stop.store(true, ordering: .relaxed)
+      usleep(505000)
     }).eraseToAnyPublisher()
   }
   
   func tryChannel<T>(_ operation: @escaping (ssh_channel) throws -> T)
   -> AnyPublisher<T, Error> where Self == AnyPublisher<ssh_channel, Error> {
-    let lock = UnfairLock()
-    var stop = false
-    
+    let stop = ManagedAtomic<Bool>(false)
+
     func loop(_ channel: ssh_channel) -> AnyPublisher<T, Error> {
       return Just(channel).tryMap { chan in
-        lock.lock()
-        if stop {
-          throw SSHError(title: "Cancelled")
-        }
         let val = try operation(chan)
-        lock.unlock()
+        if stop.load(ordering: .relaxed) {
+          throw SSHError(title: "Operation cancelled")
+        }
         return val
       }
       .tryCatch { error -> AnyPublisher<T, Error> in
+        if stop.load(ordering: .relaxed) {
+          throw SSHError(title: "Operation cancelled")
+        }
         switch error {
         case SSHError.again:
           Swift.print("tryChannel AGAIN")
           RunLoop.current.run(mode: .default, before: Date(timeIntervalSinceNow: 0.5))
-          lock.unlock()
+          if stop.load(ordering: .relaxed) {
+            throw SSHError(title: "Operation cancelled")
+          }
           return loop(channel)
         default:
           return self.tryMap { chan in
             ssh_channel_free(chan)
-            lock.unlock()
             throw error
           }.eraseToAnyPublisher()
         }
       }.eraseToAnyPublisher()
     }
     
-    return self.handleEvents(receiveCancel: {
-      lock.unlock()
-    }).flatMap(loop)
-    .handleEvents(receiveCancel: {
-      lock.spinLock()
-      stop = true
+    return self.flatMap { loop ($0) }.handleEvents(receiveCancel: {
+      stop.store(true, ordering: .relaxed)
+      usleep(505000)
     }).eraseToAnyPublisher()
   }
   
@@ -129,55 +129,54 @@ extension Publisher {
 extension AnyPublisher where Output == ssh_session, Failure == Error {
   func tryAuth(_ operation: @escaping (ssh_session) throws -> AuthState)
   -> AnyPublisher<AuthState, Error> {
-    let lock = UnfairLock()
-    var stop = false
-    
+    let stop = ManagedAtomic<Bool>(false)
+
     func loop(_ session: ssh_session) -> AnyPublisher<AuthState, Error> {
       return Just(session).tryMap { session in
-        lock.lock()
-        if stop {
-          throw SSHError(title: "Cancelled")
-        }
-
         // Check we are still connected as sometimes authentication may close from one side.
         if ssh_is_connected(session) == 0 {
           throw SSHError(title: "Disconnected", forSession: session)
         }
 
         let val = try operation(session)
-        lock.unlock()
+        if stop.load(ordering: .relaxed) {
+          throw SSHError(title: "Operation cancelled")
+        }
         return val
       }
       .tryCatch { error -> AnyPublisher<AuthState, Error> in
+        if stop.load(ordering: .relaxed) {
+          throw SSHError(title: "Operation cancelled")
+        }
         switch error {
         case SSHError.again:
           Swift.print("tryAuth AGAIN")
           RunLoop.current.run(mode: .default, before: Date(timeIntervalSinceNow: 0.5))
-          lock.unlock()
+          if stop.load(ordering: .relaxed) {
+            throw SSHError(title: "Operation cancelled")
+          }
           return loop(session)
         default:
-          lock.unlock()
           throw error
         }
       }.eraseToAnyPublisher()
     }
     
-    return self.handleEvents(receiveCancel: {
-      lock.unlock()
-    }).flatMap(maxPublishers: .max(1)) { loop($0) }
-    .flatMap { state -> AnyPublisher<AuthState, Error> in
-      // If Auth needs to continue, use the provided
-      // Publisher
-      switch state {
-      case .continue(let pub):
-        return pub
-      default:
-        return .just(state)
-      }
-    }.handleEvents(receiveCancel: {
-      lock.spinLock()
-      stop = true
-    }).eraseToAnyPublisher()
+    return self.flatMap(maxPublishers: .max(1)) { loop($0) }
+                .flatMap { state -> AnyPublisher<AuthState, Error> in
+                  // If Auth needs to continue, use the provided
+                  // Publisher
+                  switch state {
+                  case .continue(let pub):
+                    return pub
+                  default:
+                    return .just(state)
+                  }
+                }.handleEvents(receiveCancel: {
+                  stop.store(true, ordering: .relaxed)
+                  // The cancel process thread cannot move up, otherwise we get a recursive unfair_lock abort.
+                  usleep(505000)
+                }).eraseToAnyPublisher()
   }
 }
 
@@ -189,33 +188,5 @@ public extension AnyPublisher {
   
   @inlinable static func fail(error: Failure) -> Self {
     .init(Fail(error: error))
-  }
-}
-
-fileprivate class UnfairLock {
-  private var pLock: UnsafeMutablePointer<os_unfair_lock>
-  
-  init() {
-    pLock = UnsafeMutablePointer<os_unfair_lock>.allocate(capacity: 1)
-    pLock.initialize(to: os_unfair_lock())
-  }
-  
-  deinit {
-    pLock.deallocate()
-  }
-  
-  func lock() {
-    os_unfair_lock_lock(pLock)
-  }
-  
-  func unlock() {
-    os_unfair_lock_unlock(pLock)
-  }
-  
-  func spinLock() {
-    while !os_unfair_lock_trylock(pLock) {
-      // NOTE The minimum was not being acquired, so just spin the cpu.
-      // usleep(1)
-    }
   }
 }
