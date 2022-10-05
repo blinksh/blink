dfd4a2c24088611746bfc9e97f8248e4da398ff6
Yury Korolev <yury.korolev@gmail.com>
2022-10-05 12:59:33 +0300

    Remove unused code

--- main.m
+++ main.m
@@ -33,58 +33,20 @@
 #import "AppDelegate.h"
 #import <objc/runtime.h>
 
-BOOL switched = NO;
-
-@interface TestApp : UIApplication
-
-@end
-
-@implementation TestApp
-
-+ (void)initialize {
-  NSString * clsName = [@[
-      @"UIKeyboardImpl"
-  ] componentsJoinedByString:@""];
-
-  Class cls = NSClassFromString(clsName);
-
-  SEL selector = sel_getUid("canPresentPressAndHoldPopover:");
-  Method method = class_getInstanceMethod(cls, selector);
-//  IMP original = method_getImplementation(method);
-  IMP override = imp_implementationWithBlock(^BOOL(id me, void* arg0) {
-    return NO;
-  });
-  method_setImplementation(method, override);
-}
-
-//- (void)sendEvent:(UIEvent *)event {
-////  NSLog(@"event: %@", event);
-//  [super sendEvent:event];
-//}
-
-//- (BOOL)canPerformAction:(SEL)action withSender:(id)sender {
-//  NSLog(@"action %@", NSStringFromSelector(action));
-//  return YES;
-//}
-
-- (void)pressesBegan:(NSSet<UIPress *> *)presses withEvent:(UIPressesEvent *)event {
-//  NSString * v = [event valueForKeyPath:@"_modifiedInput"];
-//  if ([v isEqual:@"l"]) {
-//    switched = YES;
-//  }
-//  if (switched) {
-//    [event setValue:@"l " forKeyPath:@"_modifiedInput"];
-//  }
-//  [event setValue:@(1 << 21) forKey: @"_modifierFlags"];
-  [super pressesBegan:presses withEvent:event];
-  
-}
-
-
-@end
-
 int main(int argc, char * argv[]) {
   @autoreleasepool {
-    return UIApplicationMain(argc, argv, NSStringFromClass([TestApp class]), NSStringFromClass([AppDelegate class]));
+    Class cls = NSClassFromString(@"UIKeyboardImpl");
+    
+    SEL selector = sel_getUid("canPresentPressAndHoldPopover:");
+    Method method = class_getInstanceMethod(cls, selector);
+    //  IMP original = method_getImplementation(method);
+    IMP override = imp_implementationWithBlock(^BOOL(id me, void* arg0) {
+      return NO;
+    });
+    method_setImplementation(method, override);
+  }
+  
+  @autoreleasepool { 
+    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
   }
 }
