c37f8203f1a610aeb6665713de41c8d5db777d9f
Carlos Cabanero <carlos@blink.sh>
2024-03-26 13:37:57 -0400

    Added vim and xxd to get_frameworks

--- xcfs/Package.swift
+++ xcfs/Package.swift
@@ -34,38 +34,38 @@ var binaryTargets: [PackageDescription.Target] = [
   ),
   (
     "ios_system",
-    "f8e1364037de546809065ecdf804277fa7b95faffc32604e91ecb4de44d6294e",
-    "https://github.com/holzschu/ios_system/releases/download/v3.0.2/ios_system.xcframework.zip"
+    "50f1692873e73fd862f45f73f2c08745e822c01ff5e0a0e0aec7fed6bb946e7f",
+    "https://github.com/holzschu/ios_system/releases/download/v3.0.3/ios_system.xcframework.zip"
   ),
   (
     "awk",
-    "73abc0d502eab50e6bbdd0e49b0cf592f3a85b3843c43de6d7f42c27cde9b953",
-    "https://github.com/holzschu/ios_system/releases/download/v3.0.2/awk.xcframework.zip"
+    "428de9776d73b5ef6865b2b0057e962ebe680cff4e977d2cd038455b4728bbac",
+    "https://github.com/holzschu/ios_system/releases/download/v3.0.3/awk.xcframework.zip"
   ),
   (
     "files",
-    "d0643e2244009fc5279f1f969c6da47ca197b4e7c9dac27dea09ba0a5f1567d7",
-    "https://github.com/holzschu/ios_system/releases/download/v3.0.2/files.xcframework.zip"
+    "3224a690a41747bd85e0e5d7868979cc83884e3517b39648e1f6a171ad192e21",
+    "https://github.com/holzschu/ios_system/releases/download/v3.0.3/files.xcframework.zip"
   ),
   (
     "shell",
-    "876b709c1b76cbc1748d434fcbc2cea1aea2e281572e5fadc40244dd8a549757",
-    "https://github.com/holzschu/ios_system/releases/download/v3.0.2/shell.xcframework.zip"
+    "7c0c3321155a7e1df609566d6d4d887003cb68f5bf1bcc6eab2ca56f75f46758",
+    "https://github.com/holzschu/ios_system/releases/download/v3.0.3/shell.xcframework.zip"
   ),
   (
     "ssh_cmd",
-    "342065209123f54c92eb78a0fbda579e61948443e5f60e41d8fe356a3fe8f2ff",
-    "https://github.com/holzschu/ios_system/releases/download/v3.0.2/ssh_cmd.xcframework.zip"
+    "c7b197f5aeff4f6ba153b94d979e57916dc99a9afc9c37b394477b513f5fb8cd",
+    "https://github.com/holzschu/ios_system/releases/download/v3.0.3/ssh_cmd.xcframework.zip"
   ),
   (
     "tar",
-    "6ffe4ed265060f971df229dd1d2bff90e7bc78c80c50dcc3a0a633face440bc4",
-    "https://github.com/holzschu/ios_system/releases/download/v3.0.2/tar.xcframework.zip"
+    "8cddd932df4ea609205372c657dee827aa8422fa6d21942d9bf1c7e8148b1ab3",
+    "https://github.com/holzschu/ios_system/releases/download/v3.0.3/tar.xcframework.zip"
   ),
   (
     "text",
-    "697bee697b509d0dc8acc156a7430f453c29878d8af273adfb8902643c70ea0f",
-    "https://github.com/holzschu/ios_system/releases/download/v3.0.2/text.xcframework.zip"
+    "b1244f7612f755d5b1c04252955e37e5a8578c5cc7fd26b28b9bee3294f4e3d1",
+    "https://github.com/holzschu/ios_system/releases/download/v3.0.3/text.xcframework.zip"
   ),
   (
     "network_ios",
@@ -76,6 +76,16 @@ var binaryTargets: [PackageDescription.Target] = [
     "bc",
     "e3d72c562f726614e273efb06f6e63ccd23f9e38b14c468cf9febd4302df5fdd",
     "https://github.com/holzschu/bc/releases/download/v1.0/bc_ios.xcframework.zip"
+  ),
+  (
+    "vim",
+    "782fba9fe318a39c8cebb52ff71efc8cbc3b97ba748a5a60fe11bee6c473b32c",
+    "https://github.com/blinksh/vim/releases/download/v9.1.0187/vim.xcframework.zip"
+  ),
+  (
+    "xxd",
+    "eb44e4567287ccd13d98f10f727600e6e806abd968369e96c9b7d700276961c7",
+    "https://github.com/blinksh/vim/releases/download/v9.1.0187/xxd.xcframework.zip"
   )
 ].map { name, checksum, url in PackageDescription.Target.binaryTarget(name: name, url: url, checksum: checksum)}
 
